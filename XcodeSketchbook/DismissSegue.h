20a14d5f56e25af3275dce34a7efd53cd9925e73
rs-redmadrobot <rs@redmadrobot.com>
2015-02-19 14:46:31 +0400

    Add Auto Layout

--- XcodeSketchbook/DismissSegue.h
+++ XcodeSketchbook/DismissSegue.h
@@ -2,7 +2,7 @@
 //  DismissSegue.h
 //  XcodeSketchbook
 //
-//  Created by Roman Shelekhov on 17.02.15.
+//  Created by RS on 17.02.15.
 //  Copyright (c) 2015 RedMadRobot. All rights reserved.
 //
 
